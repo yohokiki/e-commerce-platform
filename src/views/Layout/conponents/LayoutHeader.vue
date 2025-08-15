89dc52febad8a7869c9adbe0fe0fc7e030de3e61
yohokiki <522730477@qq.com>
2025-08-15 15:25:18 +0800

    layout一级标签渲染

--- src/views/Layout/conponents/LayoutHeader.vue
+++ src/views/Layout/conponents/LayoutHeader.vue
@@ -1,7 +1,20 @@
 <script setup>
- 
+import { getCategoryAPI } from '@/apis/layout'
+import { onMounted, ref } from 'vue'
+
+const currentList = ref([])
+const getCategory = async () => {
+  const res = await getCategoryAPI()
+  // console.log(res)
+  currentList.value = res.result
+}
+
+onMounted(() => {
+  getCategory()
+})
+
 </script>
- 
+
 <template>
   <header class='app-header'>
     <div class="container">
@@ -9,12 +22,9 @@
         <RouterLink to="/">小兔鲜</RouterLink>
       </h1>
       <ul class="app-header-nav">
-        <li class="home">
-          <RouterLink to="/">首页</RouterLink>
+        <li class="home" v-for="item of currentList" :key="item.id">
+          <RouterLink to="/">{{ item.name }}</RouterLink>
         </li>
-        <li> <RouterLink to="/">居家</RouterLink> </li>
-        <li> <RouterLink to="/">美食</RouterLink> </li>
-        <li> <RouterLink to="/">服饰</RouterLink> </li>
       </ul>
       <div class="search">
         <i class="iconfont icon-search"></i>
@@ -23,8 +33,8 @@
     </div>
   </header>
 </template>
- 
- 
+
+
 <style scoped lang='scss'>
 .app-header {
   background: #fff;
@@ -33,10 +43,10 @@
     display: flex;
     align-items: center;
   }
- 
+
   .logo {
     width: 200px;
- 
+
     a {
       display: block;
       height: 132px;
@@ -45,71 +55,71 @@
       background: url('@/assets/images/logo.png') no-repeat center 18px / contain;
     }
   }
- 
+
   .app-header-nav {
     width: 820px;
     display: flex;
     padding-left: 40px;
     position: relative;
     z-index: 998;
-   
+
     li {
       margin-right: 40px;
       width: 38px;
       text-align: center;
-   
+
       a {
         font-size: 16px;
         line-height: 32px;
         height: 32px;
         display: inline-block;
-   
+
         &:hover {
           color: $xtxColor;
           border-bottom: 1px solid $xtxColor;
         }
       }
-   
+
       .active {
         color: $xtxColor;
         border-bottom: 1px solid $xtxColor;
       }
     }
   }
- 
+
   .search {
     width: 170px;
     height: 32px;
     position: relative;
     border-bottom: 1px solid #e7e7e7;
     line-height: 32px;
- 
+
     .icon-search {
       font-size: 18px;
       margin-left: 5px;
     }
- 
+
     input {
       width: 140px;
       padding-left: 5px;
       color: #666;
     }
   }
- 
+
   .cart {
     width: 50px;
- 
+
     .curr {
       height: 32px;
       line-height: 32px;
       text-align: center;
       position: relative;
       display: block;
- 
+
       .icon-cart {
         font-size: 22px;
       }
- 
+
       em {
         font-style: normal;
         position: absolute;
