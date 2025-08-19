7e33fc737950c0427963394917e7788083021c9c
yohokiki <522730477@qq.com>
2025-08-19 10:45:45 +0800

    封装小商品

--- src/views/Home/components/HomeProduct.vue
+++ src/views/Home/components/HomeProduct.vue
@@ -2,6 +2,7 @@
 import { onMounted, ref } from 'vue';
 import HomePanel from './HomePanel.vue'
 import {findGoodsAPI} from '@/apis/home'
+import GoodsItem from './GoodsItem.vue';
 
 const goodsList = ref([])
 const getFindGoods = async()=>{
@@ -26,12 +27,7 @@ onMounted(()=>getFindGoods())
         </RouterLink>
         <ul class="goods-list">
           <li v-for="good in cate.goods" :key="good.id">
-            <RouterLink to="/" class="goods-item">
-              <img v-img-lazy="good.picture" alt="" />
-              <p class="name ellipsis">{{ good.name }}</p>
-              <p class="desc ellipsis">{{ good.desc }}</p>
-              <p class="price">&yen;{{ good.price }}</p>
-            </RouterLink>
+           <GoodsItem :goods="good"/>
           </li>
         </ul>
       </div>
@@ -126,41 +122,6 @@ onMounted(()=>getFindGoods())
       }
     }
 
-    .goods-item {
-      display: block;
-      width: 220px;
-      padding: 20px 30px;
-      text-align: center;
-      transition: all .5s;
-
-      &:hover {
-        transform: translate3d(0, -3px, 0);
-        box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
-      }
-
-      img {
-        width: 160px;
-        height: 160px;
-      }
-
-      p {
-        padding-top: 10px;
-      }
-
-      .name {
-        font-size: 16px;
-      }
-
-      .desc {
-        color: #999;
-        height: 29px;
-      }
-
-      .price {
-        color: $priceColor;
-        font-size: 20px;
-      }
-    }
   }
 }
 </style>
